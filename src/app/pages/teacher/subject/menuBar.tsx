1580154119363db013ed87abe3146648ed0ce065
patarajarin <patara1919@gmail.com>
2024-12-09 14:39:58 +0700

    edit menu bar

--- src/app/pages/teacher/subject/menuBar.tsx
+++ src/app/pages/teacher/subject/menuBar.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function FilterBar() {
   return (
-    <div className="w-full py-4 bg-blue-500 px-10">
+    <div className=" mx-3 rounded-md my-2 py-4 bg-blue-900 shadow-sm shadow-gray-500 px-10">
         <div className=" text-center text-3xl text-white">รายการวิชาที่สอน</div>
       </div>
   )
-}
+ }
