242bed5ce2f7d25803dd1f4a5c1261b7e6045ecf
tirawat1 <tirawat.pst@gmail.com>
2025-01-02 20:50:38 +0700

    Admin : management Event Page

--- src/app/pages/admin/eventManagement/page.tsx
+++ src/app/pages/admin/eventManagement/page.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import { EventManagement } from "./eventManagement";
 
 export default function Page() {
   return (
     <header className="mx-4 sm:mx-10 lg:mx-44 p-4 mt-10 ">
-      <div>Event Management</div>
+      <EventManagement />
     </header>
   );
 }
