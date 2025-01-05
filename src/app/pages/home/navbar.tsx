cd76be092ebe6359ed486bb05655e640d084b4bf
tirawat1 <tirawat.pst@gmail.com>
2025-01-05 23:22:19 +0700

    fixed merge ที่ระเบิดจาก bell

--- src/app/pages/home/navbar.tsx
+++ src/app/pages/home/navbar.tsx
@@ -3,7 +3,8 @@ import React, { useState } from "react";
 import Image from "next/image";
 import { NavbarData } from "@/resource/home/navbarData";
 import DropMenu from "@/app/components/dropdown/dropdown-menu-1";
-import { CircleCheck, Menu, Shield, X } from "lucide-react";
+import { CircleCheck, LogIn, Menu, X } from "lucide-react";
+import DropDownMobile from "@/app/components/dropdown/dropdown-mobile";
 
 export function Navbar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -23,30 +24,26 @@ export function Navbar() {
               className="p-1 w-auto h-16 lg:h-20 md:h-18"
             />
             {/* Name university */}
-            <div className="flex flex-col justify-start font-inter font-bold text-sm lg:text-lg p-2 pl-6 text-[#2E58A2] ">
-              <span className="text-sm lg:text-lg sm:text-lg line-clamp-3 ">
+            <div className="flex flex-col justify-start font-inter font-bold p-2 pl-6 text-[#2E58A2]">
+              {/* Hidden for small screens */}
+              <span className="hidden md:block text-lg">
                 Ekawit Business Administration Vocational College
               </span>
-              <span className="sm:text-sm hidden lg:block md:block sm:block ">
+              <span className="text-sm">
                 วิทยาลัยอาชีวศึกษาเอกวิทย์บริหารธุรกิจ
               </span>
             </div>
           </a>
 
-          <div className="justify-end grid place-items-center px-10 lg:mr-6 md:mr-6 overflow-hidden h-fi sm:px-2 sm:py-1">
+          {/* Sign In Button */}
+          <div className="hidden sm:flex justify-end items-center">
             <a
               href="/pages/login"
-              className="overflow-hidden h-fit px-4 py-2 gap-2 flex rounded-md bg-blue-900 text-white hover:bg-blue-700 "
+              className="px-4 py-2 flex rounded-md bg-blue-900 text-white hover:bg-blue-700"
             >
-              <div className="hidden lg:block md:block line-clamp-1  h-fit ">
-                ระบบผู้ใช้
-              </div>
-              <div className="lg:hidden md:hidden ">Login</div>
-              <div className="">
-                <Shield
-                  style={{ width: "1.6rem", height: "1.5rem" }}
-                  className="text-white hidden lg:block md:block"
-                />
+              Sign In
+              <div className="mt-0.5 ml-2 text-xl">
+                <LogIn />
               </div>
             </a>
           </div>
@@ -60,19 +57,28 @@ export function Navbar() {
           </button>
         </div>
       </div>
-      <div className=" text-white  h-12 lg:h-auto md:h-auto  sm:h-auto w-full flex py-1 px-0 lg:pl-2 lg:pr-0 md:px-2 sm:px-2 justify-between bg-blue-950">
-        <div className="flex ">
-          <DropMenu menuData={NavbarData} />
+      <div
+        className={`${
+          isMenuOpen ? "block" : "hidden"
+        } sm:flex flex-col sm:flex-row bg-blue-950 text-white sm:py-1 px-6 w-full justify-between`}
+      >
+        <div className="flex flex-col sm:flex-row w-full items-center ">
+          {isMenuOpen ? (
+            <DropDownMobile menuData={NavbarData} />
+          ) : (
+            // Show DropMenu for larger screens
+            <DropMenu menuData={NavbarData} />
+          )}
         </div>
 
-        <div className="justify-end flex place-items-center lg:px-10 md:px-10 px-1 sm:px-2 sm:py-1">
+        <div className="flex flex-col sm:flex-row justify-end items-start sm:px-2 sm:py-1 m-2 w-1/6">
           <a
-            href="/pages/register"
-            className="flex gap-2 justify-center  py-1  bg-blue-950 px-10 border-white border-2 rounded-full text-white text-xl hover:bg-white hover:text-blue-950 "
+            href="/pages/login"
+            className="flex gap-2 items-center justify-center sm:py-2 px-6 bg-blue-950 border-white border-2 rounded-full text-white text-base hover:bg-blue-700"
           >
             สมัครออนไลน์
-            <div className="pt-0.5">
-              <CircleCheck className="" />
+            <div>
+              <CircleCheck />
             </div>
           </a>
         </div>
