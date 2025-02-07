7c033a881168088e512962d87d8acbe6b341f3a6
patarajarin <patara1919@gmail.com>
2025-02-07 12:14:28 +0700

    edit home-page & card แนวนอน

--- src/app/components/card/card-horizontal.tsx
+++ src/app/components/card/card-horizontal.tsx
@@ -17,59 +17,36 @@ interface CardHorizontalProps {
 
 export default function CardHorizontal({ cardData }: CardHorizontalProps) {
   return (
-    <>
+    <div className="grid lg:gap-4 py-5  ">
       {Array.isArray(cardData) &&
-        cardData.map((data, index) => (
+        cardData.map((data: any, index) => (
           <div
-            key={index}
-            className="rounded-md border-[1px] border-[#CFD0D4] bg-card text-card-foreground overflow-hidden shadow-lg  hover:bg-slate-50 group lg:w-[1000px] md:w-[900px] w-[300px] sm:w-[300px] my-3 hover:scale-[1.01] duration-500 bg-white sm:grid grid md:flex"
+            key={data.eventId}
+            className="flex rounded-md lg:w-[800px] group hover:scale-[102%] duration-500 lg:h-[180px] group bg-white  border border-gray-200 overflow-hidden shadow-lg "
           >
-            <div className="lg:flex md:flex grid sm:grid items-start ">
-              <div className="relative  overflow-hidden lg:w-5/12 lg:h-44 md:w-7/12 md:h-44 mr-4 w-full sm:w-full h-36 sm:h-44 ">
-                <Image
-                  src={data.img}
-                  alt={data.topic}
-                  fill
-                  className="absolute w-12 lg:w-full  object-cover transition group-hover:opacity-80  duration-700"
-                />
-                <div className="relative w-full h-full bg-gradient-to-t from-gray-900/30 to-gray-900/5"></div>
+            <div className="h-full relative  lg:w-60 overflow-hidden">
+              <img
+                src={data.img}
+                alt={data.topic}
+                className="absolute lg:w-60 lg:h-full object-cover transition  group-hover:opacity-80  duration-700"
+              />
+              <div className="relative w-full h-full bg-gradient-to-t from-gray-900/40 to-gray-900/5"></div>
+            </div>
+            <div className=" lg:w-[600px] group-hover:bg-gray-50 duration-500  grid  py-4 px-4">
+              <div className="flex justify-between ">
+                <div className="font-semibold text-blue-800 text-lg">
+                  {data.topic}
+                </div>
+                <div className="text-gray-700 text-lg">{data.category}</div>
               </div>
-              <div className="grid px-5 w-full">
-                <div className="flex justify-between">
-                  <span className="font-semibold text-black text-lg">
-                    {data.topic}
-                  </span>
-                  <span className="text-gray-700 text-lg">
-                    <span className="text-black font-semibold">category: </span>
-                    {data.category}
-                  </span>
-                </div>
-                <div className="mt-3 text-gray-600 line-clamp-4">
+              <div className="h-fit overflow-hidden">
+                <p className=" text-gray-600 lg:text-[16px] text-sm line-clamp-4 ">
                   {data.description}
-                </div>
-                <div className="grid w-full">
-                  <div className="flex justify-between">
-                    <span className="font-semibold text-black text-lg">
-                      {data.topic}
-                    </span>
-                    <span className="text-gray-700 text-lg">
-                      <span className="text-black font-semibold">
-                        category:{" "}
-                      </span>
-                      {data.category}
-                    </span>
-                  </div>
-                  <div className="mt-3 text-gray-600">
-                    {data.description.slice(0, 200)}...
-                  </div>
-                  <div className="text-end mt-2 font-semibold text-gray-700">
-                    {data.date}
-                  </div>
-                </div>
+                </p>
               </div>
             </div>
           </div>
         ))}
-    </>
+    </div>
   );
 }
--- src/app/pages/home/page.tsx
+++ src/app/pages/home/page.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import { cardData } from "@/resource/fetchData/cardContent";
 import CLShomepage from "@/app/components/cls/cls-homepage";
 import CardVertical from "@/app/components/card/card-vertical";
@@ -6,31 +6,42 @@ import CardHorizontal from "@/app/components/card/card-horizontal";
 
 import StickerFacebook from "@/app/components/Effect/StickerFacebook";
 import StickerYoutube from "@/app/components/Effect/StickerYoutube";
+import FadeInOnScroll from "@/app/components/Effect/FadInScroll";
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-white">
+      <StickerFacebook />
+      <StickerYoutube />
       <CLShomepage />
       <div className="flex w-full bg-white ">
-        <div className="w-full items-center  flex flex-col  px-4 py-4">
-          <StickerFacebook />
-          <StickerYoutube />
-          <h1 className="text-5xl animate-fadeIn text-blue-950 my-5  font-bold px-5 py-1">
-            News And Events
-          </h1>
-          <div className="animate-fadeIn grid lg:grid-cols-3 gap-x-20 lg:mx-10  mx-5 ">
-            <CardVertical cardData={cardData} />
-          </div>
-          <div className="w-full my-5 flex justify-center ">
-            <img
-              className="h-40 w-full lg:w-[90%] lg:h-auto"
-              src="https://www.obac.ac.th/mainsite/images/header/harder2018.jpg"
-            />
-          </div>
-          <div className="grid place-items-start mx-10 lg:px-20 md:px-10 sm:px-10 overflow-hidden lg:w-[95%]">
-            {/* <CardHorizontal cardData={cardData} /> */}
-            <CardHorizontal cardData={cardData} />
-          </div>
+        <div className="w-full items-center text-center px-4 py-4">
+          <FadeInOnScroll>
+            <h1 className="text-5xl text-blue-950 my-5  font-extrabold px-5 py-1">
+              News And Events
+            </h1>
+          </FadeInOnScroll>
+        </div>
+      </div>
+      <FadeInOnScroll>
+        <div className="grid lg:grid-cols-3 gap-x-20 lg:mx-10  mx-5 ">
+          <CardVertical cardData={cardData} />
+        </div>
+      </FadeInOnScroll>
+      <div className="w-full">
+        <FadeInOnScroll>
+          <img
+            className="h-auto w-full"
+            src="https://www.obac.ac.th/mainsite/images/header/harder2018.jpg"
+          />
+        </FadeInOnScroll>
+      </div>
+      <div className="lg:flex grid w-full gap-5 lg:px-10 px-5">
+        <div className=" h-fit  w-fit">
+          <CardHorizontal cardData={cardData} />
+        </div>
+        <div className="h-fit overflow-hidden py-5 ">
+          <div className="px-10 py-10 rounded-md  bg-blue-500">Image</div>
         </div>
       </div>
     </div>
