4ba95474332c35db118cdeb76b8f929abf1a548c
tirawat1 <tirawat.pst@gmail.com>
2025-02-14 16:00:36 +0700

    update tab active classroomByGroupId"

--- src/app/pages/academic/grading/management/classroom/main.tsx
+++ src/app/pages/academic/grading/management/classroom/main.tsx
@@ -1,17 +1,34 @@
 "use client";
 
 import { ChevronRight } from "lucide-react";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { ClassroomGrading } from "./classroom";
+import { ClassroomByGroupId } from "./classroomByGroupId";
+
+export interface ClassroomByGroupIdProps {
+  groupId: number;
+  term: string;
+  year: string;
+  classroom: string;
+}
 
 export function Main() {
   const [activeTab, setActiveTab] = useState<string>("classroom");
   const handleTab = (tab: string) => {
     if (activeTab === tab) return;
     setActiveTab(tab);
+    if (activeTab === "classroomByGroupId") {
+      setSelectedClassroomData(undefined);
+    }
   };
-  const handleSelectedData = (data: { groupId: number; room: string }) => {
-    console.log(data);
+  const [selectedClassroomData, setSelectedClassroomData] =
+    useState<ClassroomByGroupIdProps>();
+
+  const handleSelectedClassRoomDataByGroupId = (
+    data: ClassroomByGroupIdProps
+  ) => {
+    setSelectedClassroomData(data);
+    setActiveTab("classroomByGroupId");
   };
 
   return (
@@ -30,13 +47,35 @@ export function Main() {
             </button>
             <ChevronRight />
           </div>
+          {selectedClassroomData && (
+            <div className="w-full flex items-center justify-center">
+              <button
+                className="min-w-32 w-auto  hover:bg-slate-50 p-1 rounded-md"
+                onClick={() => handleTab("classroomByGroupId")}
+              >
+                <span className="text-black text-sm font-bold">
+                  {selectedClassroomData.classroom}
+                </span>
+              </button>
+              <ChevronRight />
+            </div>
+          )}
         </div>
       </div>
+
       {/* active Tab */}
       {activeTab === "classroom" && (
         <ClassroomGrading
           handleTab={handleTab}
-          handleSelectedData={handleSelectedData}
+          handleSelectedData={handleSelectedClassRoomDataByGroupId}
+        />
+      )}
+      {activeTab === "classroomByGroupId" && selectedClassroomData && (
+        <ClassroomByGroupId
+          groupId={selectedClassroomData.groupId}
+          classroom={selectedClassroomData.classroom}
+          term={selectedClassroomData.term}
+          year={selectedClassroomData.year}
         />
       )}
     </header>
