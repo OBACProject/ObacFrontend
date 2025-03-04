6ab1a36f95e8675d98989cc2fce836fc12c38432
patarajarin <patara1919@gmail.com>
2025-03-04 23:24:54 +0700

    update token in api

--- src/api/user/userAPI.tsx
+++ src/api/user/userAPI.tsx
@@ -1,12 +1,22 @@
 import { GetAllAcademicUser } from "@/dto/userDto";
+import { cookies } from "next/headers";
 
 export const fetchGetAllAcademicUser = async (): Promise<
   GetAllAcademicUser[]
 > => {
   try {
+    const token = cookies().get("token")?.value;
 
     const response = await fetch(
       `${process.env.NEXT_PUBLIC_API_URL_V1}/api/Academic/GetAllAcademicUserAsync`
+      ,
+      {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+      }
     );
     if (!response.ok) {
       throw new Error("Failed to get teacher data");
