504f03572acc6600da2609b9e9cf734b7b288d4b
patarajarin <patara1919@gmail.com>
2025-03-31 20:28:09 +0700

    fetch update from test-build

--- src/app/components/credit.tsx
+++ src/app/components/credit.tsx
@@ -2,7 +2,7 @@ import { School } from "lucide-react";
 
 export default function Credit() {
   return (
-    <div className="w-full bg-[#010b76] px-5 py-10 lg:px-10">
+    <div className="w-full bg-[#0C243C] px-5 py-10 lg:px-10">
       <div className="grid place-items-center  gap-5 lg:gap-10 lg:justify-center md:justify-center lg:flex md:flex ">
         <div className="flex text-white">
           <div className="rounded-full hidden lg:block md:block sm:block w-fit h-fit bg-white px-0 py-1">
--- src/app/pages/home/page.tsx
+++ src/app/pages/home/page.tsx
@@ -29,22 +29,50 @@ export default function Home() {
       </div>
       <div className="w-full  items-center text-center px-4 ">
         <OpeningScroll>
-          <h1 className="lg:text-5xl  text-3xl text-blue-950   font-extrabold px-5 py-1">
+          <h1 className="lg:text-4xl  text-3xl text-blue-950   font-extrabold px-5 py-1">
             Ekawit Business Administration Vocational College (OBAC)
           </h1>
         </OpeningScroll>
       </div>
       <OpeningScroll>
-        <div className="grid place-items-center my-5 w-full">
+        <div className="grid place-items-center mt-10 mb-5 w-full">
           <hr className=" w-11/12 border border-blue-950" />
         </div>
       </OpeningScroll>
-
+      <FadeInOnScroll>
+        <div className="w-full grid place-items-center">
+          <h1 className="text-5xl text-blue-950 my-5  font-extrabold px-5 py-1">
+            Welcome to OBAC
+          </h1>
+          <i className="w-[60%] text-blue-950 text-lg">
+            "At the heart of our university lies a powerful vision — to shape
+            the future by merging innovation with entrepreneurship. As a leading
+            institution in technology and business, we equip students with
+            cutting-edge skills, critical thinking, and real-world experience.
+            Through dynamic programs, collaborative research, and
+            industry-driven education, we empower the next generation of leaders
+            to thrive in a rapidly changing world. Here, every idea matters, and
+            every student is a catalyst for progress. Join us where technology
+            meets opportunity — and futures are built."
+          </i>
+        </div>
+      </FadeInOnScroll>
+      <div className="w-full mt-10 py-2 bg-blue-950"></div>
+      <FadeInOnScroll>
+        <div className=" relative group flex items-center justify-center  lg:w-[100%] md:w-[100%] md:h-[400px] lg:h-[600px] w-full h-[220px] sm:w-[100%] sm:h-[300px] overflow-hidden">
+          <img
+            className="absolute lg:w-[100%] lg:h-[600px] sm:w-[100%] sm:h-[300px] md:w-[100%] md:h-[400px] w-full h-[220px] object-cover duration-1000 group-hover:scale-[102%]"
+            src="/banner/banner.jpg"
+          />
+          <div className="relative w-full h-full bg-gradient-to-t from-gray-900/60 to-gray-900/0"></div>
+        </div>
+      </FadeInOnScroll>
+      <div className="w-full mb-5 py-2 bg-blue-950"></div>
       <div className="flex w-full bg-white ">
         <div className="w-full items-center text-center px-4 ">
           <FadeInOnScroll>
             <h1 className="text-5xl text-blue-950 my-5  font-extrabold px-5 py-1">
-              News And Events
+              ข่าวสารและกิจกรรม
             </h1>
           </FadeInOnScroll>
         </div>
@@ -54,20 +82,12 @@ export default function Home() {
           <CardVertical cardData={cardData} />
         </div>
       </FadeInOnScroll>
-      <FadeInOnScroll>
-        <div className="relative group flex items-center justify-center  lg:w-[100%] md:w-[100%] md:h-[400px] lg:h-[400px] w-full h-[220px] sm:w-[100%] sm:h-[300px] overflow-hidden">
-          <img
-            className="absolute lg:w-[100%] lg:h-[400px] sm:w-[100%] sm:h-[300px] md:w-[100%] md:h-[400px] w-full h-[220px] object-cover duration-1000 group-hover:scale-[102%]"
-            src="/images/obac_view.jpg"
-          />
-          <div className="relative w-full h-full bg-gradient-to-t from-gray-900/40 to-gray-900/5"></div>
-        </div>
-      </FadeInOnScroll>
+
       <div className="lg:flex grid w-full gap-5 lg:px-10 px-5">
         <div className=" h-fit  w-fit">
           <CardHorizontal cardData={cardData} />
         </div>
-        <FadeInOnScroll>
+        {/* <FadeInOnScroll>
           <div className="h-fit grid grid-cols-2 gap-3 overflow-hidden py-5 px-10 ">
             <div className="px-20 py-40 rounded-md  bg-gradient-to-tr from-emerald-200  to-blue-800">
               For Post
@@ -95,9 +115,8 @@ export default function Home() {
               For Post
             </div>
           </div>
-        </FadeInOnScroll>
+        </FadeInOnScroll> */}
       </div>
     </div>
   );
 }
-
