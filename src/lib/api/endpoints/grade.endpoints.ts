f90ba6bd78bdbc8b623fe1157598175342d0cfc0
Tirawat <tirawat.p@tcc-technology.com>
2025-06-29 23:43:44 +0700

    fix: update grade endpoints to remove redundant API prefix

--- src/lib/api/endpoints/grade.endpoints.ts
+++ src/lib/api/endpoints/grade.endpoints.ts
@@ -1,9 +1,9 @@
 export const GRADE_ENDPOINTS = {
-  GET_STUDENT_GRADE_BY_TERM_YEAR: '/api/Grade/GetStudentGradeByTermYear',
-  GET_STUDENT_IF_GRADE_BELOW: '/api/Grade/GetStudentIfGradeBelow',
-  GET_GROUP_GRADE_ABOVE: '/api/Grade/GetGroupGradeAbove',
-  GET_STUDENT_TRANSCRIPT: '/api/Grade/GetStudentTranscript',
-  GET_GROUP_SUMMARY_GRADE: '/api/Grade/GetGroupSummaryGrade',
-  GET_GROUP_SUMMARY_GRADE_AVAILABLE_STATUS: '/api/Grade/GetGroupSummaryGradeAvailableStatus',
-  GET_STUDENT_GROUP_GRADE_BY_GROUP_ID_TERM_YEAR: '/api/Grade/GetStudentGroupGradeByGroupIdTermYear',
+  GET_STUDENT_GRADE_BY_TERM_YEAR: '/Grade/GetStudentGradeByTermYear',
+  GET_STUDENT_IF_GRADE_BELOW: '/Grade/GetStudentIfGradeBelow',
+  GET_GROUP_GRADE_ABOVE: '/Grade/GetGroupGradeAbove',
+  GET_STUDENT_TRANSCRIPT: '/Grade/GetStudentTranscript',
+  GET_GROUP_SUMMARY_GRADE: '/Grade/GetGroupSummaryGrade',
+  GET_GROUP_SUMMARY_GRADE_AVAILABLE_STATUS: '/Grade/GetGroupSummaryGradeAvailableStatus',
+  GET_STUDENT_GROUP_GRADE_BY_GROUP_ID_TERM_YEAR: '/Grade/GetStudentGroupGradeByGroupIdTermYear',
 } as const;
