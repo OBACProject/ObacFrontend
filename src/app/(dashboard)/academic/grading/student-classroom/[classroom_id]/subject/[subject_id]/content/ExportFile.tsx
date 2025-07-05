0bbd40e67aa6bcac11ec140a70be40c359da98a2
Tirawat <tirawat.p@tcc-technology.com>
2025-07-05 16:29:23 +0700

    feat: enhance grading functionality with editable grade table, advanced filters, and export options

---
+++ src/app/(dashboard)/academic/grading/student-classroom/[classroom_id]/subject/[subject_id]/content/ExportFile.tsx
@@ -0,0 +1,108 @@
+// === components/grad/ExportFile.tsx ===
+"use client";
+import React from "react";
+// import { GetGradBySubjectId, GetSubjectBySubjectId } from "@/dto";
+import GenSubjectScore from "@/lib/PDF/genSubjectScore";
+import GenStudentNameInSubject from "@/lib/PDF/genStudentNameInSubject";
+import {
+  ConvertClassroomToExcelWithSubject,
+  ConvertScoreToExcel,
+} from "@/lib/convertToExcel";
+import { GetGradBySubjectId } from "@/dto/gradDto";
+import { GetSubjectBySubjectId } from "@/dto/subjectDto";
+import { Button } from "@/components/ui/button";
+
+interface ExportFileProps {
+  grads: GetGradBySubjectId[];
+  subject: GetSubjectBySubjectId;
+  roomName: string;
+  term: string;
+  year: string;
+}
+
+export default function ExportFile({
+  grads,
+  subject,
+  roomName,
+  term,
+  year,
+}: ExportFileProps) {
+  const convertGrad = grads.map((item) => {
+    const prefix = item.gender === "Male" ? "นาย" : "นางสาว";
+    return {
+      studentCode: item.studentCode,
+      name: `${prefix} ${item.firstName} ${item.lastName}`,
+      collectScore: item.collectScore,
+      testScore: item.testScore,
+      affectiveScore: item.affectiveScore,
+      totalScore: item.collectScore + item.testScore + item.affectiveScore,
+    };
+  });
+
+  const convertStudentExcel = grads.map((item) => {
+    const prefix = item.gender === "Male" ? "นาย" : "นางสาว";
+    return {
+      studentCode: item.studentCode,
+      name: `${prefix} ${item.firstName} ${item.lastName}`,
+    };
+  });
+
+  return (
+    <div className="flex flex-row flex-wrap gap-2">
+      <Button
+        className="text-sm bg-[#e4f1f8] text-gray-600 hover:bg-gray-200 rounded-md px-4 py-2"
+        onClick={() =>
+          GenSubjectScore({
+            grads,
+            studentGroup: roomName,
+            subjectId: subject?.subjectCode,
+            subjectName: subject?.subjectName,
+          })
+        }
+      >
+        ดาวน์โหลดใบคะแนน PDF
+      </Button>
+      <Button
+        className="text-sm bg-[#e4f1f8] text-gray-600 hover:bg-gray-200 rounded-md px-4 py-2"
+        onClick={() =>
+          GenStudentNameInSubject({
+            grads,
+            studentGroup: roomName,
+            subjectId: subject?.subjectCode,
+            subjectName: subject?.subjectName,
+          })
+        }
+      >
+        ดาวน์โหลดรายชื่อ PDF
+      </Button>
+      <Button
+        className="text-sm bg-[#e4f1f8] text-gray-600 hover:bg-gray-200 rounded-md px-4 py-2"
+        onClick={() =>
+          ConvertScoreToExcel(
+            convertGrad,
+            term,
+            year,
+            subject?.subjectCode ?? "",
+            subject?.subjectName ?? "",
+            roomName ?? ""
+          )
+        }
+      >
+        ดาวน์โหลดใบคะแนน Excel
+      </Button>
+      <Button
+        className="text-sm bg-[#e4f1f8] text-gray-600 hover:bg-gray-200 rounded-md px-4 py-2"
+        onClick={() =>
+          ConvertClassroomToExcelWithSubject(
+            convertStudentExcel,
+            subject?.subjectCode ?? "",
+            subject?.subjectName ?? "",
+            roomName ?? ""
+          )
+        }
+      >
+        ดาวน์โหลดใบรายชื่อ Excel
+      </Button>
+    </div>
+  );
+}
