be086958427580ff44024cc225bb07546f0e1405
Tirawat <tirawat.p@tcc-technology.com>
2025-07-16 03:47:18 +0700

    feat: add request and response interfaces for dashboard, enrollment, program, and student group

---
+++ src/lib/api/models/dashboard/dashboard.response.ts
@@ -0,0 +1,6 @@
+
+
+export interface DashboardGetGenderInfoCountResponse {
+    gender : string; 
+    count : number; 
+}
---
+++ src/lib/api/models/program/program.request.ts
@@ -0,0 +1,8 @@
+
+
+export interface CreateProgramRequest {
+    facultyId: number;
+    programName: string;
+    subProgramName?: string;
+    class: string;
+}
