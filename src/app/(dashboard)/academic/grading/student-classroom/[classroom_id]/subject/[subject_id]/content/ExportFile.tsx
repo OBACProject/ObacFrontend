d25cfbb3e6733fe1e834e50e02f8e80dd5212735
Tirawat <tirawat.p@tcc-technology.com>
2025-07-20 00:35:50 +0700

    refactor: comment out Excel export button for future implementation

--- src/app/(dashboard)/academic/grading/student-classroom/[classroom_id]/subject/[subject_id]/content/ExportFile.tsx
+++ src/app/(dashboard)/academic/grading/student-classroom/[classroom_id]/subject/[subject_id]/content/ExportFile.tsx
@@ -70,7 +70,7 @@ export default function ExportFile({
       >
         ดาวน์โหลดรายชื่อ PDF
       </Button>
-      <Button
+      {/* <Button
         className="text-sm bg-[#e4f1f8] text-gray-600 hover:bg-gray-200 rounded-md px-4 py-2"
         onClick={() =>
           ConvertScoreToExcel(
@@ -84,7 +84,7 @@ export default function ExportFile({
         }
       >
         ดาวน์โหลดใบคะแนน Excel
-      </Button>
+      </Button> */}
       <Button
         className="text-sm bg-[#e4f1f8] text-gray-600 hover:bg-gray-200 rounded-md px-4 py-2"
         onClick={() =>
