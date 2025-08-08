f17a9abf12d2a67349a0f3107291718ab3da6f8b
Tirawat <tirawat.pst@gmail.com>
2025-08-08 20:30:03 +0700

    fix: Correct property name from 'remarks' to 'remark' in grade response and update related rendering logic

--- src/lib/api/models/grade/grade.response.ts
+++ src/lib/api/models/grade/grade.response.ts
@@ -145,7 +145,7 @@ export interface GetStudentDetailAndSummaryScoreByStudentCodeResponse {
     finaltermScore: number;
     totalScore: number;
     finalGrade: number | null;
-    remarks: string | null;
+    remark: string | null;
     subjectName: string;
     subjectCode: string;
     credit: number;
