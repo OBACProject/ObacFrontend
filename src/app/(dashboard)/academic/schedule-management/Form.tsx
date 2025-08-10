b8b5b59387ad9c3d65f7c707e1fd4e27c1c09bb1
patarajarin.n <patara1919@gmail.com>
2025-08-10 22:28:43 +0700

    edit prefix teacher

--- src/app/(dashboard)/academic/schedule-management/Form.tsx
+++ src/app/(dashboard)/academic/schedule-management/Form.tsx
@@ -80,7 +80,7 @@ export default function Form() {
     {
       label: "ชื่อ",
       width: "20%",
-      render: (item) => `${item.prefix || "นาย,นาง"} ${item.firstName} `,
+      render: (item) => `${item.prefix || "อาจารย์"} ${item.firstName} `,
       className: "text-start lg:pl-6",
     },
     {
