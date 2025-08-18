2a7351a058ba31045e828fa3e13e57af708ef72a
patarajarin.n <patara1919@gmail.com>
2025-08-18 10:36:11 +0700

    edit

--- src/app/(dashboard)/teacher/profile/page.tsx
+++ src/app/(dashboard)/teacher/profile/page.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Image from "next/image";
 import ProfileForm from "./Form";
 // import Notice from "./Notice";
 
@@ -6,27 +7,27 @@ export default function page() {
   return (
     <div
       className=" bg-blue-200"
-    //   style={{
-    //     backgroundImage: `
-    //   /* เส้นตั้ง */
-    //   repeating-linear-gradient(
-    //     to right,
-    //     rgba(255, 255, 255, 1) 0px,
-    //     rgba(255, 255, 255, 1) 1px,
-    //     transparent 1px,
-    //     transparent 20px
-    //   ),
-    //   /* เส้นนอน */
-    //   repeating-linear-gradient(
-    //     to bottom,
-    //     rgba(255, 255, 255, 1) 0px,
-    //     rgba(255, 255, 255, 1) 1px,
-    //     transparent 1px,
-    //     transparent 20px
-    //   )
-    // `,
-    //     backgroundSize: "20px 20px",
-    //   }}
+      style={{
+        backgroundImage: `
+        /* เส้นตั้ง */
+        repeating-linear-gradient(
+          to right,
+          rgba(255, 255, 255, 1) 0px,
+          rgba(255, 255, 255, 1) 1px,
+          transparent 1px,
+          transparent 20px
+        ),
+        /* เส้นนอน */
+        repeating-linear-gradient(
+          to bottom,
+          rgba(255, 255, 255, 1) 0px,
+          rgba(255, 255, 255, 1) 1px,
+          transparent 1px,
+          transparent 20px
+        )
+      `,
+        backgroundSize: "20px 20px",
+      }}
     >
       <div className="relative w-full flex items-center justify-center min-h-[calc(80dvh-2rem)]  overflow-hidden">
         <div className="pointer-events-none absolute inset-0 ">
@@ -35,7 +36,14 @@ export default function page() {
              bg-gradient-to-br from-sky-400/60 via-cyan-300/60 to-emerald-300/60
              mix-blend-multiply right-40
              animate-blob-22-pulse-12
-             [animation-delay:0s,0s]" 
+             [animation-delay:0s,0s]"
+          />
+          <div
+            className="absolute size-86 rounded-full
+             bg-gradient-to-br from-sky-400/60 via-cyan-300/60 to-emerald-300/60
+             mix-blend-multiply left-40
+             animate-blob-22-pulse-12
+             [animation-delay:0s,0s]"
           />
 
           <div
@@ -66,7 +74,7 @@ export default function page() {
           <ProfileForm />
         </div>
 
-        <div className="py-5 px-10 shadow-md  backdrop-blur-sm bg-white/20 grid place-items-start  rounded-md absolute bottom-5 right-10">
+        <div className="py-5 px-10 shadow-md  backdrop-blur-sm bg-white/60 grid place-items-start  rounded-md absolute bottom-5 right-10">
           <h1 className="font-semibold ">หากระบบมีปัญหาสามารถติดต่อมาได้ที่</h1>
           <p className="text-gray-600">patara1919@gmail.com</p>
           <p className="text-gray-600">tel. 091-864-9154</p>
