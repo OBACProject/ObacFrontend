4b2d2fc265f824fc0e3207552ae42e4297955a87
patarajarin.n <patara1919@gmail.com>
2025-08-18 12:06:11 +0700

    edit some error

--- src/app/(dashboard)/student/schedule/page.tsx
+++ src/app/(dashboard)/student/schedule/page.tsx
@@ -1,16 +1,13 @@
 "use client";
 import { useEffect, useState } from "react";
 
-
-
 import Form from "./Form";
+import { LoaderCircle } from "lucide-react";
 
 export default function Page() {
   const [isLoading, setLoading] = useState<boolean>(false);
   const [studentId, setStudentID] = useState<string>("");
-  useEffect(() => {
-
-  }, []);
+  useEffect(() => {}, []);
   useEffect(() => {
     setLoading(true);
   }, [studentId]);
@@ -20,9 +17,11 @@ export default function Page() {
         <Form student_id={Number(studentId)} />
       ) : (
         <div className="px-10  grid place-items-center py-4">
-          <div className="w-full h-full bg-white border-[1px] border-blue-400 rounded-xl py-5 lg:py-10 flex gap-5 lg:gap-10 items-center justify-center h-fit">
+          <div className="w-full h-full bg-white border-[1px] border-blue-400 rounded-xl py-5 lg:py-10 flex gap-5 lg:gap-10 items-center justify-center ">
             <LoaderCircle className="w-12 h-12 text-blue-400 animate-spin" />
-            <h1 className="text-xl text-gray-600 font-prompt">กำลังโหลดข้อมูล... </h1>
+            <h1 className="text-xl text-gray-600 font-prompt">
+              กำลังโหลดข้อมูล...{" "}
+            </h1>
           </div>
         </div>
       )}
