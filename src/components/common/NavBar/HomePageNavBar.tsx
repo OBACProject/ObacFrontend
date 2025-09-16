0d90a7fae63586c6be29bf3744552a1438c3a669
patarajarin.n <patara1919@gmail.com>
2025-09-16 13:11:26 +0700

    edit nav

--- src/components/common/NavBar/HomePageNavBar.tsx
+++ src/components/common/NavBar/HomePageNavBar.tsx
@@ -69,8 +69,6 @@ export function HomePageNavBar() {
             : "hidden"
         )}
       >
-
-
         <div className="w-full">
           {isMobile ? (
             <DropDownMobile menuData={NavbarData} />
@@ -81,13 +79,6 @@ export function HomePageNavBar() {
 
         {isMobile && (
           <div className="flex flex-col sm:flex-row gap-2 mt-3 md:mt-0 justify-end items-center w-full md:w-auto">
-            {/* <Link
-              href="/register"
-              className="flex items-center gap-2 px-6 py-1 bg-[#143d66] border-white border-2 rounded-full text-white text-base hover:bg-white hover:text-blue-900 duration-500 font-prompt"
-            >
-              สมัครออนไลน์
-              <CircleCheck className="w-5 h-5" />
-            </Link> */}
             <Link
               href="/login"
               className="flex items-center gap-2 px-6 py-1 border border-white rounded-full text-white text-base hover:bg-white hover:text-black duration-500 font-prompt_Light"
