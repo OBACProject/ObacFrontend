053fda7eadec627ed14039ca396a13932a9f2e84
Mhing <natthap411@gmail.com>
2025-09-22 16:58:59 +0700

    add Docker CI workflow and refactor API calls to use publicRuntimeConfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
-}
+	output: "standalone",
+	publicRuntimeConfig: {
+		NEXT_PUBLIC_API_URL_V2: process.env.NEXT_PUBLIC_API_URL_V2,
+		NEXT_PUBLIC_SECRET_KEY: process.env.NEXT_PUBLIC_SECRET_KEY,
+	},
+};
 
 export default nextConfig;
--- src/resource/academics/grading/api/individualGradeApiData.tsx
+++ src/resource/academics/grading/api/individualGradeApiData.tsx
@@ -1,51 +1,52 @@
 "use server";
 import { GetAllStudent, StudentTranscriptData } from "@/dto/studentDto";
 import { cookies } from "next/headers";
-
+import getConfig from "next/config";
+const { publicRuntimeConfig } = getConfig();
 export async function GetAllStudentDataApi(): Promise<GetAllStudent[]> {
-  const token = cookies().get("token")?.value;
-  try {
-    const response = await fetch(
-      `${process.env.NEXT_PUBLIC_API_URL_V1}/Student/GetAllStudent`,
-      {
-        method: "GET",
-        headers: {
-          Authorization: `Bearer ${token}`,
-          "Content-Type": "application/json",
-        },
-      }
-    );
-    const text = await response.text();
-    const json = JSON.parse(text);
-    const data: GetAllStudent[] = json.data;
-    return data;
-  } catch (err) {
-    console.error(err);
-    throw new Error("Failed to get student data");
-  }
+	const token = cookies().get("token")?.value;
+	try {
+		const response = await fetch(
+			`${publicRuntimeConfig.NEXT_PUBLIC_API_URL_V1}/Student/GetAllStudent`,
+			{
+				method: "GET",
+				headers: {
+					Authorization: `Bearer ${token}`,
+					"Content-Type": "application/json",
+				},
+			}
+		);
+		const text = await response.text();
+		const json = JSON.parse(text);
+		const data: GetAllStudent[] = json.data;
+		return data;
+	} catch (err) {
+		console.error(err);
+		throw new Error("Failed to get student data");
+	}
 }
 
 export async function GetStudentByIdDataApi(
-  id: number
+	id: number
 ): Promise<StudentTranscriptData> {
-  const token = cookies().get("token")?.value;
-  try {
-    const response = await fetch(
-      `${process.env.NEXT_PUBLIC_API_URL_V1}/Student/GetStudentGradeDetail?studentId=${id}`,
-      {
-        method: "GET",
-        headers: {
-          Authorization: `Bearer ${token}`,
-          "Content-Type": "application/json",
-        },
-      }
-    );
-    const text = await response.text();
-    const json = JSON.parse(text);
-    const data: StudentTranscriptData= json.data;
-    return data;
-  } catch (err) {
-    console.error(err);
-    throw new Error("Failed to get student data");
-  }
+	const token = cookies().get("token")?.value;
+	try {
+		const response = await fetch(
+			`${publicRuntimeConfig.NEXT_PUBLIC_API_URL_V1}/Student/GetStudentGradeDetail?studentId=${id}`,
+			{
+				method: "GET",
+				headers: {
+					Authorization: `Bearer ${token}`,
+					"Content-Type": "application/json",
+				},
+			}
+		);
+		const text = await response.text();
+		const json = JSON.parse(text);
+		const data: StudentTranscriptData = json.data;
+		return data;
+	} catch (err) {
+		console.error(err);
+		throw new Error("Failed to get student data");
+	}
 }
