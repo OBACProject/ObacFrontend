2053aaf2ce7a0e195ebe9b8a3880dc659878bf6a
patarajarin.n <patara1919@gmail.com>
2025-09-23 13:57:26 +0700

    update bug homepage

--- src/app/(home)/structure/management/page.tsx
+++ src/app/(home)/structure/management/page.tsx
@@ -1,17 +1,7 @@
-"use client";
-import OrganizationalChart, { type NodeData } from "./organizational-chart";
-import raw from "./organization-data.json";
-import HeaderHomePageMenu from "@/components/common/Header/Header";
+import React from 'react'
 
-export default function Page() {
-  const data = raw as unknown as NodeData[];
-
+export default function page() {
   return (
-    <div className="pt-[70px] sm:pt-[70px] md:pt-[140px]">
-      <div className="bg-white text-blue-950 font-prompt">
-              <HeaderHomePageMenu title="โครงสร้างการบริหารงาน" />
-            </div>
-      <OrganizationalChart data={data} height="78vh" />
-    </div>
-  );
+    <div>page</div>
+  )
 }
