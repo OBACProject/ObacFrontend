77411ea14310c066b9e44d39e8fc54a2ee86f6fa
Tirawat <tirawat.pst@gmail.com>
2025-09-26 00:21:28 +0700

    fix: correct query parameter name for publish status update in student group service

--- src/lib/api/services/studentGroup.service.ts
+++ src/lib/api/services/studentGroup.service.ts
@@ -24,8 +24,7 @@ export class StudentGroupService extends BaseService {
         return this.put<void>(STUDENT_GROUP_ENDPOINTS.UPDATE_STUDENT_GROUP_BY_STUDENT_GROUP_ID, params);
     }
     async updatePublishStatusByStudentGroupId(studentGroupId: number, isPublished: boolean): Promise<void> {
-        const data = this.put<void>(`${STUDENT_GROUP_ENDPOINTS.PUT_IS_PUBLISHED_BY_STUDENT_GROUP_ID}?studentGroupId=${studentGroupId}&isPublished=${isPublished}`, {});
-        return this.put<void>(`${STUDENT_GROUP_ENDPOINTS.PUT_IS_PUBLISHED_BY_STUDENT_GROUP_ID}?studentGroupId=${studentGroupId}&isPublished=${isPublished}`, {});
+        return this.put<void>(`${STUDENT_GROUP_ENDPOINTS.PUT_IS_PUBLISHED_BY_STUDENT_GROUP_ID}?studentGroupId=${studentGroupId}&publish=${isPublished}`, {});
     }
 }
 
